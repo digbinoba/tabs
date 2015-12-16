ea04f55ced3831e060fcb769b237a1bdfc29e416
Igbinoba,Delano A <digbinoba@msl-100-5.lab.at.ufl.edu>
2015-12-16 08:23:23 -0500

    Initial Commit

---
+++ tabs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tabs
+//
+//  Created by Igbinoba,Delano A on 12/16/15.
+//  Copyright © 2015 Igbinoba,Delano A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
